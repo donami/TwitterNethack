7c610597ae813ae84b80001395d59dd160d2918c
Markus Hederström <d0namiz@gmail.com>
2017-05-14 11:58:18 +0200

    Code cleanup

--- src/com/bth/Main.java
+++ src/com/bth/Main.java
@@ -5,14 +5,7 @@ import com.bth.App.Application;
 public class Main
 {
     public static void main(String[] args) {
-        String BLACK = "\u001B[0;30m";
-        String RED = "\u001B[0;31m";
         String GREEN = "\u001B[0;32m";
-        String YELLOW = "\u001B[0;33m";
-        String BLUE = "\u001B[0;34m";
-        String MAGENTA = "\u001B[0;35m";
-        String CYAN = "\u001B[0;36m";
-        String WHITE = "\u001B[0;37m";
 
         System.out.println(GREEN);
 
